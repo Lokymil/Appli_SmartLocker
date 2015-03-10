04dac924e0660146c8cce5770d0187f898198c2e
Shannow13 <mescle@et.esiea.fr>
2015-03-10 19:18:08 +0100

    JSON fonctionne mais renvoie un PENDING des ténèbres...

---
+++ SmartLocker/app/src/main/java/pst/esiea/com/smartlocker/JSONParser.java
@@ -0,0 +1,70 @@
+package pst.esiea.com.smartlocker;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;import java.lang.String;import java.lang.StringBuilder;
+
+/**
+ * Created by pierre-jean on 10/03/15.
+ */
+public class JSONParser {
+
+    static JSONArray jarray = null;
+;
+
+    public JSONArray getJSONFromUrl(String url){
+
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+
+        try{
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+
+            if(statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+            }else{
+
+                }
+            }
+
+         catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try{
+            jarray = new JSONArray(builder.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return jarray;
+    }
+
+}
