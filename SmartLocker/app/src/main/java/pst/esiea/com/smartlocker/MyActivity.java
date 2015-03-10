cf9a55825752718788b6f7660fa6ebf85f92b427
Lokymil <arrestierchristophe@gmail.com>
2015-03-10 10:10:05 +0100

    About

--- SmartLocker/app/src/main/java/pst/esiea/com/smartlocker/MyActivity.java
+++ SmartLocker/app/src/main/java/pst/esiea/com/smartlocker/MyActivity.java
@@ -33,14 +33,26 @@ public class MyActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void openSearch() {
+
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
     }
 
 
